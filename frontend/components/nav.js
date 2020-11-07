dbb28935f00aa07ccb6d8d85986cadbd192ca403
lipowada <lipowada@fel.cvut.cz>
2020-11-07 18:43:40 +0100

    update

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -9,7 +9,7 @@ const Nav = ({ categories }) => {
           <ul className="uk-navbar-nav">
             <li>
               <Link href="/">
-                <a>Strapi Blog</a>
+                <a>CZM FEL CVUT</a>
               </Link>
             </li>
           </ul>
